726e4f2a459642e82018f0fa4c888b4e5eaabdbd
rasmi <rasminoel@gmail.com>
2023-09-20 15:40:10 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return 'This one is on me!';
   }
-  else if (distance > 2000) {
+  else if (1999 < distance && distance < 2500) {
     return 'I will gladly take your thirty bucks.';
   }
-  else if (distance > 2500) {
+  else {
     return 'No can do.';
   }
 
